3f6110d0d9349413c9b850cd41f003a42da72203
Rodrigo Batista <rodrigo.s.batista@outlook.com>
2021-07-31 09:53:38 -0300

    Aula 08, comparação de Strings com objetos utilizando o operador ternario

---
+++ Aula08/ComparaçãoString/src/comparaçãostring/ComparaçãoString.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comparaçãostring;
+
+/**
+ *
+ * @author rodri
+ */
+public class ComparaçãoString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nome1 = "Rodrigo";
+        String nome2 = "Rodrigo";
+        String nome3 = new String("Rodrigo");
+        String res;
+        
+        res = (nome1.equals(nome3))?"igual":"diferente";
+        
+        System.out.println(res);
+        
+        
+        
+    }
+    
+}
