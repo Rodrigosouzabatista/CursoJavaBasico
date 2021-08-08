245179789de70bbd256b6aa36855277865f58f80
Rodrigo Batista <rodrigo.s.batista@outlook.com>
2021-08-08 19:01:48 -0300

    Estrutura de Repetição 3, utilizamos a estrutura for(c = 1; c <= 3; c++){ }

---
+++ Aula10/RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author rodri
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cc,jj;
+        
+        for(cc = 1; cc <= 3; cc++){
+            for (jj = 1; jj <= 3;jj++){
+            System.out.println(cc +" : " +jj);
+            }
+    }
+    }
+    
+}
